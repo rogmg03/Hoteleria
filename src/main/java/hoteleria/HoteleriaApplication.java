a740c0306a3b644c8ab34c195872622274d31af9
Roger <rogermora03@gmail.com>
2024-11-17 17:03:02 -0600

    First Commit

---
+++ src/main/java/hoteleria/HoteleriaApplication.java
@@ -0,0 +1,13 @@
+package hoteleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteleriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hoteleria/HoteleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package hoteleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
